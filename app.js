01823802ff85a5f57e5d5b4171ad862e63a4b8ed
wilko55 <andyjwilko@gmail.com>
2016-12-05 07:15:08 +0000

    Resent headers error

--- app.js
+++ app.js
@@ -25,19 +25,20 @@ app.post('/', function(req,res, next) {
   if (req.body.attending === '' || req.body.name === '' || req.body.numberAttending === ''){
     res.render('index', {error: true})
   }
-
-  console.log(req.body)
-  models.Guest.create({
-    'name': req.body.name,
-    'attending': req.body.attending,
-    'dietary': req.body.dietary,
-    'dietaryText': req.body.dietaryText,
-    'numberAttending': req.body.numberAttending,
-    'song1': req.body.song1,
-    'song2': req.body.song2,
-  }).then(function () {
-    res.render('index', {thanks: true})
-  });
+  else {
+    console.log(req.body)
+    models.Guest.create({
+      'name': req.body.name,
+      'attending': req.body.attending,
+      'dietary': req.body.dietary,
+      'dietaryText': req.body.dietaryText,
+      'numberAttending': req.body.numberAttending,
+      'song1': req.body.song1,
+      'song2': req.body.song2,
+    }).then(function () {
+      res.render('index', {thanks: true})
+    });
+  }
 })
 
 app.get('/registry', function(req,res, next) {
@@ -48,28 +49,6 @@ app.get('/venues/name/:id', function(req,res, next) {
   var venues = JSON.parse(require('fs').readFileSync('./venues.json')).venues[req.params.id]
 
   res.json(venues)
-})
-
-app.get('/venues/capacity/:num', function(req,res, next) {
-  var venues = JSON.parse(require('fs').readFileSync('./venues.json'))
-  var matches = {}
-  for (var prop in venues.venues) {
-    console.log(prop + ' - ' + venues.venues[prop].capacity)
-    if (venues.venues[prop].capacity <= req.params.num) {
-      matches[prop] = venues.venues[prop]
-    }
-  }
-  res.json(matches)
-})
-
-app.post('/churlish', function(req,res, next) {
-
-  if (req.body.password === 'videoman'){
-    res.send('yo')
-  }
-  else {
-    res.render('churlish', {showPass: true, showVid: false})
-  }
 })
 
 app.listen(app.get('port'), function() {
